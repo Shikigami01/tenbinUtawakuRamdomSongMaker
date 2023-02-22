c4f8e7e92a5ff15bee4c8bd4c45c6f65d869afe4
HouraijiN <>
2023-02-23 02:07:56 +0900

    jsの修正

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ import json2 from '../json/songList2.json';
 import json3 from '../json/songList3.json';
 import json4 from '../json/songList4.json';
 
+// jsonを全部結合
+const jsonAll = [];
+jsonAll.push(...json1, ...json2, ...json3, ...json4);
+
 const selectSongRandom = (buttonName, textName, jsonName) => {
   const slotButton1 = document.getElementById(buttonName);
   const songResult1 = document.getElementById(textName);
@@ -15,6 +19,7 @@ const selectSongRandom = (buttonName, textName, jsonName) => {
   }
 };
 
+selectSongRandom('js-slotButton0', 'js-resultSongName1', jsonAll);
 selectSongRandom('js-slotButton1', 'js-resultSongName1', json1);
 selectSongRandom('js-slotButton2', 'js-resultSongName2', json2);
 selectSongRandom('js-slotButton3', 'js-resultSongName3', json3);
